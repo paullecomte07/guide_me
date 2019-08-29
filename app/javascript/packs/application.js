64f9ce3e01b70052b75d8ff6eb14478231c43932
Paul Lecomte <paullecomte07@gmail.com>
2019-08-29 17:33:59 +0100

    bug fixes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,9 @@ import { initMapbox } from '../plugins/init_mapbox';
 import { detectPopup } from '../plugins/detect_popup';
 import { traceLineMap } from '../plugins/GeoJSON_line';
 import { colorPin } from '../plugins/change_color_pin';
+import { disableButton } from '../plugins/disabled_button';
+
 initMapbox();
 detectPopup();
 colorPin();
+disableButton();
