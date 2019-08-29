f094293112c00ea072879f360003c9cc02c97b86
Paul Lecomte <paullecomte07@gmail.com>
2019-08-29 13:35:56 +0100

    now the pin have a color

--- app/javascript/plugins/GeoJSON_line.js
+++ app/javascript/plugins/GeoJSON_line.js
@@ -9,40 +9,38 @@ let json = JSON.parse(a)
 let urlCoordinates = ""
 json.forEach((element)=>
   urlCoordinates += `${element[0]},${element[1]};`
-  )
+)
 urlCoordinates = urlCoordinates.slice(0,-1)
 
 fetch(`https://api.mapbox.com/directions/v5/mapbox/cycling/${urlCoordinates}?access_token=${process.env.MAPBOX_API_KEY}&geometries=geojson`)
     .then(response => response.json())
     .then(data => data["routes"])
-    .then(geometry =>
-          map.on('load', function () {
-            map.addLayer({
-              "id": "route",
-              "type": "line",
-              "source": {
-                "type": "geojson",
-                "data": {
-                  "type": "Feature",
-                  "properties": {},
-                  "geometry": geometry["0"]["geometry"]
-                  }
-                },
-                "layout": {
-                  "line-join": "round",
-                  "line-cap": "round"
-                },
-                "paint": {
-                  "line-color": "#888",
-                  "line-width": 8
+    .then(geometry => {
+      map.on('load', function () {
+        map.addLayer({
+          "id": "route",
+          "type": "line",
+          "source": {
+            "type": "geojson",
+            "data": {
+              "type": "Feature",
+              "properties": {},
+              "geometry": geometry["0"]["geometry"]
               }
-            });
-          })
-
-
-    );
-
-    //want to add [geometry["0"]["distance"],geometry["0"]["duration"]]
+            },
+            "layout": {
+              "line-join": "round",
+              "line-cap": "round"
+            },
+            "paint": {
+              "line-color": "#888",
+              "line-width": 8
+          }
+        });
+      })
+      const moreInfo = `<p>Distance: ${(geometry["0"]["distance"]/1000).toFixed(2)} km, Duration: ${Math.round(geometry["0"]["duration"]/60)} min</p>`
+      document.querySelector("#stop").insertAdjacentHTML("beforeend", moreInfo)
+    });
 
 }
 export { traceLineMap };
--- app/javascript/plugins/detect_popup.js
+++ app/javascript/plugins/detect_popup.js
@@ -1,31 +1,31 @@
 const detectPopup = () => {
 
   document.querySelector("#map").addEventListener('click', (event) => {
-  let elementStop = document.getElementById("stop");
-  let elementOrder = document.querySelector(".order-counter");
-  let elementImage = document.querySelector(".preview-picture");
-  while (elementStop.firstChild) {
-        elementStop.removeChild(elementStop.firstChild);
-      }
-  if(elementOrder.firstChild) {elementOrder.firstChild.remove()};
-  if(elementImage.firstChild) {elementImage.firstChild.remove()};
+    let elementStop = document.getElementById("stop");
+    let elementOrder = document.querySelector(".order-counter");
+    let elementImage = document.querySelector(".preview-picture");
+    while (elementStop.firstChild) {
+          elementStop.removeChild(elementStop.firstChild);
+        }
+    if(elementOrder.firstChild) {elementOrder.firstChild.remove()};
+    if(elementImage.firstChild) {elementImage.firstChild.remove()};
 
 
-  let contentPopup = document.querySelector(".mapboxgl-popup-content");
+    let contentPopup = document.querySelector(".mapboxgl-popup-content");
 
-  if(contentPopup) {
+    if(contentPopup) {
 
-    let name = contentPopup.querySelector("#stop-name").outerHTML;
-    let description = contentPopup.querySelector("#stop-description").outerHTML;
-    let image = contentPopup.querySelector("#stop-image").outerHTML;
-    let order = contentPopup.querySelector("#stop-order").outerHTML;
-    document.querySelector("#stop").insertAdjacentHTML('afterbegin', description )
-    document.querySelector("#stop").insertAdjacentHTML('afterbegin', name )
-    document.querySelector(".order-counter").insertAdjacentHTML('afterbegin', order )
-    document.querySelector(".preview-picture").insertAdjacentHTML('afterbegin', image )
-  };
+      let name = contentPopup.querySelector("#stop-name").outerHTML;
+      let description = contentPopup.querySelector("#stop-description").outerHTML;
+      let image = contentPopup.querySelector("#stop-image").outerHTML;
+      let order = contentPopup.querySelector("#stop-order").outerHTML;
+      document.querySelector("#stop").insertAdjacentHTML('afterbegin', description )
+      document.querySelector("#stop").insertAdjacentHTML('afterbegin', name )
+      document.querySelector(".order-counter").insertAdjacentHTML('afterbegin', order )
+      document.querySelector(".preview-picture").insertAdjacentHTML('afterbegin', image )
+    };
 
-});
+  });
 
 
 }
