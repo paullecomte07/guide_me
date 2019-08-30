bb7264e3cb279b40d6182a5ecd3e25e2cc5fd6e6
Paul Lecomte <paullecomte07@gmail.com>
2019-08-30 14:23:16 +0100

    add feature color on the pin

--- app/javascript/packs/plugins/change_color_pin.js
+++ app/javascript/packs/plugins/change_color_pin.js
@@ -5,13 +5,20 @@ const colorPin = () => {
       element.addEventListener('click',(event)=> {
 
         let fill = document.querySelectorAll(".mapboxgl-marker")[0].querySelector("g").children[1]
-console.log(fill.getAttribute(`fill`))
-        if (fill.getAttribute(`fill`) === 'red'){
-          event.currentTarget.querySelector("g").children[1].setAttribute(`fill`, `#3FB1CE`)
-        }
-        if (fill.getAttribute(`fill`) === '#3FB1CE'){
-          event.currentTarget.querySelector("g").children[1].setAttribute(`fill`, `red`)
-        }
+
+
+        document.querySelectorAll(".mapboxgl-marker").forEach((element)=>{
+          element.querySelector("g").children[1].setAttribute(`fill`, `#3FB1CE`)
+        })
+
+        event.currentTarget.querySelector("g").children[1].setAttribute(`fill`, `red`)
+
+
+
+
+
+
+
       })
     })
 
