d2d38226c0923e2d6542c748c6762e2319408350
Paul Lecomte <paullecomte07@gmail.com>
2019-09-02 12:49:19 +0100

    put the map in the path veiw

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,18 +1,15 @@
 class StopsController < ApplicationController
-  def index
-    guide = Guide.find(params[:guide_id])
-    @stops = Stop.where(guide: guide)
-    @stopsMarker = @stops.map do |stop|
-      [stop.longitude,stop.latitude
-      ]
-    end
-    @markers = @stops.map do |stop|
-      {
-        lat: stop.latitude,
-        lng: stop.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { stop: stop })
-      }
-    end
-  end
-end
 
+
+  def index
+  end
+
+
+  def new
+    @stop = Stop.new()
+  end
+
+  def create
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,12 @@ Rails.application.routes.draw do
 
   resources :guides do
     resources :reviews, only: [:new, :create]
-    resources :stops, only: [:index]
+    resources :stops, only: [:index, :new, :create]
     resources :wishes, only: [:create, :destroy ]
     resources :guide_tags, only: [:create, :destroy ]
     resources :itineraries, only: [:new, :create]
     get 'path', to: 'guides#path', as: 'path'
+    post
   end
   resources :reviews, only: [:destroy]
 
