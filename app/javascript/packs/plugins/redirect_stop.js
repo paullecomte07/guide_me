b9659eb0d13c7c3fca02f1282f275a30622cfaac
Paul Lecomte <paullecomte07@gmail.com>
2019-09-04 16:46:15 +0100

    change the map display

--- app/javascript/packs/plugins/redirect_stop.js
+++ app/javascript/packs/plugins/redirect_stop.js
@@ -1,6 +1,10 @@
 
 const redirectStop = () => {
-  document.getElementById("stop").addEventListener("click", (event)=> {event.currentTarget.classList.toggle("stops-popup-page")})
+  document.getElementById("stop")
+    .addEventListener("click", (event)=> {
+      event.currentTarget.classList.toggle("stops-popup-page")
+      event.currentTarget.querySelector('.preview-picture').classList.toggle("open");
+    })
 }
 
 redirectStop();
