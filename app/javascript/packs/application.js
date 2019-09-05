5ee9a74a91fba3c58c56e9f3865ba4cd2dc37c9c
Mohammad Gholami <gholamipersonal@gmail.com>
2019-09-05 11:56:54 +0100

    styled

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,12 @@ initMapbox();
 colorPin();
 disableButton();
 
+if(/Android [4-6]/.test(navigator.appVersion)) {
+   window.addEventListener("resize", function() {
+      if(document.activeElement.tagName=="INPUT" || document.activeElement.tagName=="TEXTAREA") {
+         window.setTimeout(function() {
+            document.activeElement.scrollIntoViewIfNeeded();
+         },0);
+      }
+   })
+}
